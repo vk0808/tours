d55907809c48f611a47e745b1a9a69e6b8050832
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-23 17:53:44 +0530

    displayed fetched data

--- src/Tour.js
+++ src/Tour.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
-const Tour = () => {
+const Tour = ({ id, image, info, price, name }) => {
   return (
-    <h1>Hello</h1>
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">$ {price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
   );
 };
 
