50f7da262956d1cea0829b7a011d8d24e19de1f0
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-23 17:48:06 +0530

    added header and mapped to Tour.js

--- src/Tours.js
+++ src/Tours.js
@@ -1,8 +1,20 @@
 import React from "react";
 import Tour from './Tour';
 
-const Tours = () => {
-  return <Tour />;
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div classsName="underline"></div>
+      </div>
+      <div>
+        {tours.map(tour => {
+          return <Tour key={tour.id} {...tour}/>
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
