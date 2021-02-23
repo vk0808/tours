bb63204f185a82f65d7fe2914721dec24b231b18
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-23 18:03:58 +0530

    added remove toggle and read more toggle

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [read, setRead] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -10,7 +11,13 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">$ {price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {read ? info : `${info.substring(0, 150)}... `}
+          <button onClick={() => setRead(!read)}>
+            {" "}
+            {!read ? "read more" : "show less"}
+          </button>
+        </p>
         <button className="delete-btn">not interested</button>
       </footer>
     </article>
