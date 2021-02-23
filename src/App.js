b10f6526cf363645550eb08ee585f4565f8aef3c
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-23 17:19:40 +0530

    added loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
+import React, { useState, useEffect } from 'react';
 import Tours from './Tours';
+import ReactLoading from 'react-loading';
 
 const url = 'https://course-api.com/react-tours-project';
 
 export default function App() {
+  const [isLoading, setisLoading] = useState(true);
+  
   return (
-    <Tours />
+    <div className="App">
+      { isLoading ? <ReactLoading type={'bars'} color={'blue'} /> : <Tours /> }
+    </div>
   );
 };
