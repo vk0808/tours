ea32811f12fa11847247d4d98b2e91ec0635b639
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-23 16:56:26 +0530

    added Tours.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./styles.css";
+import Tours from './Tours';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Tours />
   );
-}
+};
